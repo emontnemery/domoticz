11f9a25a4801e0797c0a285224e1db572c164da8
emontnemery <erik@montnemery.com>
2018-03-12 10:59:47 +0100

    adjust travis deploy settings

--- copyfiles.sh
+++ copyfiles.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 export SSHPASS="$FTP_PASSWORD"
-sshpass -e sftp -o StrictHostKeyChecking=no -q -P $FTP_PORT $FTP_USER@$FTP_HOST <<EOF
+sshpass -e sftp -o StrictHostKeyChecking=no -q -P "$FTP_PORT" "$FTP_USER"@"$FTP_HOST" <<EOF
   quit
 EOF
 export FTP_DIR="/incoming/domoticz/${TRAVIS_BRANCH}/${TRAVIS_JOB_NUMBER}"
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "cd $FTP_DIR || mkdir -p $FTP_DIR; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "cd \x22$FTP_DIR\x22 || mkdir -p \x22$FTP_DIR\x22; bye"
 export FTP_FILE=domoticz_${TRAVIS_OS_NAME}_${TARGET_ARCHITECTURE}.tgz
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O $FTP_DIR $FTP_FILE; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O \x22$FTP_DIR\x22 \x22$FTP_FILE\x22; bye"
 export FTP_FILE=domoticz_${TRAVIS_OS_NAME}_${TARGET_ARCHITECTURE}.tgz.sha256sum
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O $FTP_DIR $FTP_FILE; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O \x22$FTP_DIR\x22 \x22$FTP_FILE\x22; bye"
 export FTP_FILE=version_${TRAVIS_OS_NAME}_${TARGET_ARCHITECTURE}.h
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O $FTP_DIR $FTP_FILE; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O \x22$FTP_DIR\x22 \x22$FTP_FILE\x22; bye"
 export FTP_FILE=history_${TRAVIS_OS_NAME}_${TARGET_ARCHITECTURE}.txt
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O $FTP_DIR $FTP_FILE; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O \x22$FTP_DIR\x22 \x22$FTP_FILE\x22; bye"
 export FTP_FILE=History.txt
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O $FTP_DIR $FTP_FILE; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O \x22$FTP_DIR\x22 \x22$FTP_FILE\x22; bye"
 echo "$TRAVIS_COMMIT" > SHAID.txt
 export FTP_FILE=SHAID.txt
-lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O $FTP_DIR $FTP_FILE; bye"
+lftp sftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST":"$FTP_PORT" -e "put -e -O \x22$FTP_DIR\x22 \x22$FTP_FILE\x22; bye"
